08a2e68ef9892df02630b14a5295fbd58ee73e79
cade <65869535+ctrlcade@users.noreply.github.com>
2021-05-24 17:56:23 +0100

    main.lua formatting

--- main.lua
+++ main.lua
@@ -5,8 +5,7 @@ WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
 --[[ love.load function is called on game startup, it's used to setup objects,
-variables & other game requirements so the game world can be prepared properly.
-]]
+variables & other game requirements so the game world can be prepared properly. ]]
 function love.load()
 	love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
 		fullscreen = false,
@@ -16,13 +15,12 @@ function love.load()
 end
 
 --[[ love.draw function called each frame after the update function, responsible
-for drawing objects & more to the game screen
-]]
+for drawing objects & more to the game screen ]]
 function love.draw()
 	love.graphics.printf(
-		'Hello Pong!', 			-- text to be rendered on screen
-		0, 						-- starting X coord (set to 0 as will be centered)
+		'Hello Pong!', 		-- text to be rendered on screen
+		0, 			-- starting X coord (set to 0 as will be centered)
 		WINDOW_HEIGHT / 2 - 6,  -- starting Y coord (halfway down the game screen)
-		WINDOW_WIDTH,			-- number of pixels to center the text in (width of screen)
-		'center')				-- mode of allignment, set to 'center' as we want the text centered
+		WINDOW_WIDTH,		-- number of pixels to center the text in (width of screen)
+		'center')		-- mode of allignment, set to 'center' as we want the text centered
 end
