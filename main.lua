3cca6572a43fffb2fe4517ef58f83796b3311aed
cade <65869535+ctrlcade@users.noreply.github.com>
2021-05-28 20:56:34 +0100

    Paddles can now be moved
    'W' & 'S' for P1, 'UP' & 'DOWN' for P2

--- main.lua
+++ main.lua
@@ -15,6 +15,9 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+-- speed at which the paddle will move at, this is multiplied by dt (deltaTime) in update
+PADDLE_SPEED = 200
+
 --[[ love.load function is called on game startup, it's used to setup objects, 
 variables & other game requirements so the game world can be prepared properly
 ]]
@@ -27,6 +30,9 @@ function love.load()
 	-- a 'retro' styled font which can be used for any text used in the application
 	retroFont = love.graphics.newFont('font.ttf', 8)
 
+	-- larger version of the 'retro' font for drawing the score onto the screen
+	scoreFont = love.graphics.newFont('font.ttf', 32)
+
 	-- sets LÖVE's active font to the one specified in the retroFont object
 	love.graphics.setFont(retroFont)
 
@@ -37,6 +43,38 @@ function love.load()
 		resizable = false,
 		vsync = true
 	})
+
+	-- initialises score variables which are used for keeping track of the scores
+	-- of each  of the players and determining the winner, these are rendered on screen
+	player1Score = 0
+	player2Score = 0
+
+	-- paddle positions are fixed to the Y axis so they can only move upwards or downwards
+	player1Y = 30
+	player2Y = VIRTUAL_HEIGHT - 50
+end
+
+--[[ runs with each frame and takes 'dt' as parameter which is the deltaTime 
+that has elapsed since the last frame, this is already supplied by LÖVE 
+]]
+function love.update(dt)
+	-- player 1 movement handling
+	if love.keyboard.isDown('w') then
+		-- add negative paddle speed (up = -Y) to the current Y multiplied by deltaTime (dt)
+		player1Y = player1Y + -PADDLE_SPEED * dt
+	elseif love.keyboard.isDown('s') then
+		-- add positive paddle speed (down = Y) to the current Y multiplied by deltaTime (dt)
+		player1Y = player1Y + PADDLE_SPEED * dt
+	end
+
+	-- player 2 movement handling
+	if love.keyboard.isDown('up') then
+		-- add negative paddle speed (up = -Y) to the current Y multiplied by deltaTime (dt)
+		player2Y = player2Y + -PADDLE_SPEED * dt
+	elseif love.keyboard.isDown('down') then
+		-- add positive paddle speed (down = Y) to the current Y multiplied by deltaTime (dt)
+		player2Y = player2Y + PADDLE_SPEED * dt
+	end
 end
 
 -- keyboard handling, called by LÖVE every frame and passes key pressed as parameter
@@ -59,23 +97,29 @@ function love.draw()
 	love.graphics.clear(0.5, 0.51, 0.8, 1)
 
 	-- displays a welcome message near the top of the screen
+	love.graphics.setFont(retroFont)
 	love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH,'center')
 
-	-- paddles are rectangles which are drawn on the screen at certain positions, this is true about the ball too
+	-- draws the scores on the left and right centres of the screen
+	-- have to switch the currently used font before rendering
+	love.graphics.setFont(scoreFont)
+	love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50,
+		VIRTUAL_HEIGHT / 3)
+	love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30,
+		VIRTUAL_HEIGHT / 3)
 
 	-- setting paddle & ball colour to pink
 	love.graphics.setColor(1, 0.75, 0.79)
 
 	-- renders the left side's paddle
-	love.graphics.rectangle('fill', 10, 30, 5, 20)
+	love.graphics.rectangle('fill', 10, player1Y, 5, 20)
 
 	-- renders the right side's paddle
-	love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+	love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
 
 	-- renders the ball in the center of the screen
 	love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
 	-- ends the rendering at the set virtual resolution
 	push:apply('end')
-
 end
