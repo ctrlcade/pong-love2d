c0dc6f0e7afbc8e63d44ffc6b48f8d3e2820d008
cade <65869535+ctrlcade@users.noreply.github.com>
2021-05-29 14:44:54 +0100

    collides method added

--- Ball.lua
+++ Ball.lua
@@ -14,7 +14,26 @@ function Ball:init(x, y, width, height)
     -- these variables are used to keep track of the ball's velocity on both the
     -- X & Y axis since the ball can move in two dimensions
     self.dy = math.random(2) == 1 and 100 or -100
-    self.dx = math.random(-50, 50)
+    self.dx = math.random(2) == 1 and math.random(-80, -100) or math.random(80, 100)
+end
+
+-- takes a paddle object as an argument and returns either true or false depending
+-- on whether their rectangles overlap (collide)
+function Ball:collides(paddle)
+    -- first, check to see if the left edge of either of the rectangles is farther to the
+    -- right than the right edge of the other
+    if self.x > paddle.x + paddle.width or paddle.x > self.x + self.width then
+        return false
+    end
+
+    -- then check to see if the bottom edge of either of the rectangles is higher than
+    -- the top edge of the other
+    if self.y > paddle.y + paddle.height or paddle.y > self.y + self.height then
+        return false
+    end
+
+    -- if the above statements aren't true then they're overlapping (colliding)
+    return true
 end
 
 -- positions the ball on the middle of the screen with an initial random velocity
